509aae770b7107cce2d8de31601187fe2bd0318c
TOMMYGUISEPE <TommySpotifyzc@gmail.com>
2024-08-10 17:49:32 -0500

    Ajustes

--- Build.sh
+++ Build.sh
@@ -3,5 +3,22 @@
 echo " "
 echo "Wellcome to Build LICH ROM"
 echo " "
+echo "Preparing directories"
+echo " "
+#Variables de los directorios
+DIR="$(git rev-parse --show-toplevel)"
+TOOLS="$DIR/Tools"
+SCRIPTS="$P_DIR/Scripts"
+BUILD="$P_DIR/Build"
+RECSOURCE="$P_DIR/Resources"
+echo "Main variables ready"
+echo " "
+#Prueba
+cd "$P_DIR"
+cd "$TOOLS"
+#cd $SCRIPTS && touch hello.sh
+#cd $BUILD && touch hello.sh
+#cd $RECSOURCE && touch hello.sh
 
-SRC_DIR="$(git rev-parse --show-toplevel)"
+#End Build
+echo "Build Finish"
